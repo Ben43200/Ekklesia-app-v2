ed0825d44d184cf6b6127bbdf8ec287a283fb8a1
meteoben <meteoben@gmail.com>
2024-02-28 11:45:19 +0100

    ajout de h1 en const et suppression des commentaires

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,26 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-// import { ReactComponent as Hamburger } from '../../assets/icons/hamburger.svg'
-// import { ReactComponent as Brand } from '../logo.svg'
 import logo from "../utils/Ekklêsia-web-2.webp";
 import React from "react";
 import { useEffect, useRef } from "react";
-// import logo from '../logo-png-mini2.webp'
-
 import "./navbar.scss";
-// import { Squash as Hamburger } from "hamburger-react";
-// import { Squeeze as Hamburger } from 'hamburger-react'
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
   let menuRef = useRef();
 
   const handleShowNavbar = () => {
-    // début modif scroll
-    // window.scrollTo(0,0)
-    //fin modif scroll
-
     setShowNavbar(!showNavbar);
   };
+
   const closeAndTop = () => {
     window.scrollTo(0, 0);
-    // setShowNavbar(!showNavbar);
   };
-  // const [open, setOpen] = React.useState(false);
 
-  // const handleOpen = () => {
-  //   setOpen(!open);
-  // };
   useEffect(() => {
     let handler = (e) => {
       if (!menuRef.current.contains(e.target)) {
         setShowNavbar(false);
-        // console.log(menuRef.current);
       }
     };
     document.addEventListener("mousedown", handler);
@@ -45,10 +30,6 @@ const Navbar = () => {
     };
   });
 
-  // const closeAndTop = () => {
-  //   window.scrollTo(0, 0);
-  //   setShowNavbar(!showNavbar);
-  // };
   const menuItemsData = [
     { title: "Accueil", url: "" },
     { title: "Notre mission", url: "mission" },
@@ -57,14 +38,14 @@ const Navbar = () => {
     { title: "Nos réalisations", url: "realisations" },
     { title: "Contact", url: "contact" },
   ];
+  const title = "Avec Ekklêsia Web, dynamisez votre vie d'église avec le Net !";
+
   return (
     <header>
       <nav className="navbar">
         <div className="container">
           <img src={logo} className="logo-1" alt="logo" />
-
           <div className="menu-icon" onClick={handleShowNavbar}>
-            {/* <Hamburger /> */}
             <span className="burger-icon" ref={menuRef}>
               <span></span>
               <span></span>
@@ -72,56 +53,22 @@ const Navbar = () => {
             </span>
           </div>
           <div className={`nav-elements  ${showNavbar && "active"}`}>
-            {/* <ul>
-              <li>
-                <NavLink to="/" onClick={closeAndTop}>
-                  Accueil
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/mission" onClick={closeAndTop}>
-                  Notre Mission
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/methodologie" onClick={closeAndTop}>
-                  Notre Méthodologie
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/services" onClick={closeAndTop}>
-                  Nos Services
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/realisations" onClick={closeAndTop}>
-                  Nos Réalisations
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact" onClick={closeAndTop}>
-                  Contact
-                </NavLink>
-              </li>
-            </ul> */}
-              <ul className="menus">
-             
-
-        {menuItemsData.map((menu, index) => {
-          return (
-            <li className="menu-items" key={index}>
-              <NavLink to={`/${menu.url}`} onClick={closeAndTop}>{menu.title}
-              </NavLink>
-            </li>
-           
-          );
-        })}
-      </ul>
+            <ul className="menus">
+              {menuItemsData.map((menu, index) => {
+                return (
+                  <li className="menu-items" key={index}>
+                    <NavLink to={`/${menu.url}`} onClick={closeAndTop}>
+                      {menu.title}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </nav>
       <div className="title-container">
-        <h1>Avec Ekklêsia Web, dynamisez votre vie d'église avec le Net !</h1>
+        <h1>{title}</h1>
       </div>
     </header>
   );
