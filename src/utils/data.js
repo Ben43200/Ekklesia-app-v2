7c214951d64e51e0eaa5d36d35884daa320252a5
meteoben <meteoben@gmail.com>
2024-02-28 16:02:08 +0100

    data.js ok url photo apparait sur home plus ajout de diapositive dans utils

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,32 +1,23 @@
 export const listings = [
     {
       "id": 1,
-      "description": "heater",
+      "description": "vue sortie temple",
       "image": require("../utils/vue_sortie_temple.jpg")
     },
     {
       "id": 2,
-      "description": "2019 Toyota Tacoma grill",
-      "image": "./images/toyota-grill.jpg"
+      "description": "vue expo",
+      "image": require("../utils/vue_expo_grd_format.jpg")
+    },
+    {
+      "id": 3,
+      "description": "vue console etage",
+      "image": require("../utils/vue_console_etage.jpg")
     },
     {
       "id": 4,
-      "description": "FREE Hundreds of DVD/CD Cases - Different Sizes and Colors",
-      "image": "./images/dvd-cases.jpg"
-    },
-    {
-      "id": 5,
-      "description": "wood",
-      "image": "./images/wood.jpg"
-    },
-    {
-      "id": 6,
-      "description": "Beautiful couch",
-      "image": "./images/couch.jpg"
-    },
-    {
-      "id": 7,
-      "description": "Treadmill Parts For Free",
-      "image": "./images/treadmill.jpg"
+      "description": "vue vitrail etage",
+      "image": require("../utils/vue_vitrail_etage.jpg")
     }
+
   ]
